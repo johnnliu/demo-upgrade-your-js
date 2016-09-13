dbfc04cbeb590ad4564e37c543cae22dbe3862fe
John Liu <johnnliu@gmail.com>
2016-09-13 23:07:23 +1000

    added webhook example

---
+++ azure-function-web-hook/funky-azure-function-sp-webhook.js
@@ -0,0 +1,114 @@
+var request = require("request");
+var adal = require("adal-node")
+var fs = require("fs")
+
+module.exports = function (context, req) {
+    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+
+    var authorityHostUrl = 'https://login.microsoftonline.com';
+    var tenant = 'johnliu365.onmicrosoft.com';
+    var authorityUrl = authorityHostUrl + '/' + tenant;
+    var resource = 'https://johnliu365.sharepoint.com';
+    var clientId = process.env['MyClientId'];
+    var thumbprint = process.env['MyThumbPrint'];
+    var certificate = fs.readFileSync(__dirname + '/funky.pem', { encoding: 'utf8' });
+
+    //context.log(context);
+    //context.log(certificate);
+
+    var authContext = new adal.AuthenticationContext(authorityUrl);
+
+    authContext.acquireTokenWithClientCertificate(resource, clientId, certificate, thumbprint, function (err, tokenResponse) {
+        if (err) {
+            context.log('well that didn\'t work: ' + err.stack);
+            context.done();
+        } else {
+            //context.log(tokenResponse);
+
+            var options = null;
+            var headers = {
+                'Authorization': 'Bearer ' + accesstoken,
+                'Accept': 'application/json;odata=nometadata',
+                'Content-Type': 'application/json'
+            };
+
+            if (req.body) {
+                context.log(req.body);
+            }
+
+            var accesstoken = tokenResponse.accessToken;
+
+            if (req) {
+                if (req.query && req.query.validationtoken) {
+                    // if validationtoken is specified in query
+                    // immediately return token as text/plain
+                    context.log(req.query);
+                    context.log(req.query.validationtoken);
+                    context.res = { "content-type": "text/plain", body: req.query.validationtoken };
+                    context.done();
+                    return;
+                }
+                if (req.body && req.body.sub) {
+                    // if request with body.sub 
+                    // post a subscription request
+                    options = {
+                        method: 'POST',
+                        uri: "https://johnliu365.sharepoint.com/_api/web/lists/getbytitle('subscribe-this')/subscriptions",
+                        body: JSON.stringify({
+                            "resource": "https://johnliu365.sharepoint.com/_api/web/lists/getbytitle('subscribe-this')",
+                            "notificationUrl": "https://johnno-funks.azurewebsites.net/api/poke-spo2?code=q8sq9wxm62asd-YOURTRIGGERCODE",
+                            "expirationDateTime": "2017-01-01T16:17:57+00:00",
+                            "clientState": "jonnofunks"
+                        }),
+                        headers: headers
+                    };
+                    request(options, function (error, res, body) {
+                        context.log(error);
+                        context.log(body);
+                        context.res = { body: body || '' };
+                        context.done();
+                    });
+                    return;
+                }
+                if (req.body && req.body.subs) {
+                    // if request with body.subs 
+                    // GET subscriptions request
+                    options = {
+                        method: 'GET',
+                        uri: "https://johnliu365.sharepoint.com/_api/web/lists/getbytitle('subscribe-this')/subscriptions",
+                        headers: headers
+                    };
+                    request(options, function (error, res, body) {
+                        context.log(error);
+                        context.log(body);
+                        context.res = { body: body || '' };
+                        context.done();
+                    });
+                    return;
+                }
+                // there really should be a DELETE request to remove the subscription
+            }
+
+            // default action.  
+            // create a list item in a different list "Poked"
+            options = {
+                method: "POST",
+                uri: "https://johnliu365.sharepoint.com/_api/web/lists/getbytitle('Poked')/items",
+                body: JSON.stringify({ '__metadata': { 'type': 'SP.Data.PokedListItem' }, 'Title': 'Hello, ' + ((req.body ? req.body.name : null) || "test!") }),
+                headers: headers
+            };
+
+            context.log(options);
+            request(options, function (error, res, body) {
+                context.log(error);
+                context.log(body);
+                context.res = { body: body || '' };
+                context.done();
+            });
+        }
+    });
+
+
+
+
+};
